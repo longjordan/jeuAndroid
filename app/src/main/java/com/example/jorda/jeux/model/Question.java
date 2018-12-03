9ea7dfa812cb7e761cb083d8183cc8462773a005
Pierre Demolliens <pierredemol@hotmail.fr>
2018-12-04 00:09:39 +0100

    Essaye amélioration questions.

--- app/src/main/java/com/example/jorda/jeux/model/Question.java
+++ app/src/main/java/com/example/jorda/jeux/model/Question.java
@@ -1,10 +1,21 @@
 package com.example.jorda.jeux.model;
 
+import android.content.IntentFilter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class Question {
 
     private int image;
     private String reponse;
+    private ArrayList<String> reponsesOk = new ArrayList<>();
 
+    public Question(int image, ArrayList<String> reponsesOk){
+        this.image=image;
+        this.reponsesOk =reponsesOk;
+    }
     public Question(int image, String reponse) {
         this.image = image;
         this.reponse = reponse;
